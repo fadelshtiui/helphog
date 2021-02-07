fa0047747ec06dc064dcacbc65cda3397c10f284
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2021-02-07 13:30:43 -0800

    Deleted duplicate javascript files

--- loading5.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', function () {
-         	$('#loading-animation').css('display', 'none');
-            $('#loading-animation').fadeIn(900);
-            $('a').css('display', 'none');
-            $('a').fadeIn(900);
-            $('.form').css('display', 'none');
-            $('.form').delay( 400 ).fadeIn(900);
-});
--- verify.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    
-	const urlParams = new URLSearchParams(window.location.search)
-    id('message').innerText = urlParams.get('message');
-    
-});
