4e58e8de7e5440fed6f9608117f25b5c00a5158d
Fadel Shtiui <fadelshtiui@gmail.com>
2021-02-16 13:29:28 -0800

    combined all the loading.js files

--- js/loading3.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', function () {
-         	$('#loading-animation').css('display', 'none');
-            $('#loading-animation').fadeIn(900);
-            $('#logo').css('display', 'none');
-            $('#logo').fadeIn(900);
-            $('main').css('display', 'none');
-            $('main').delay( 400 ).fadeIn(900);
-});
