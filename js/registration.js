8be2dd0884cdaba49f37724da6781989b7283d8d
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2021-03-25 17:30:28 -0700

    Changed icons to fa-fa

--- js/registration.js
+++ js/registration.js
@@ -3,27 +3,10 @@
 (function () {
 
     window.addEventListener('load', function () {
-        id('left').onclick = form;
-        id('right').onclick = application;
-
-        id("provider").classList.add("hidden");
-
-        let data = new FormData();
-        data.append("session", getSession())
-        let url = "php/session.php"
-        fetch(url, { method: "POST", body: data })
-            .then(checkStatus)
-            .then(res => res.json())
-            .then(updateNav)
-            .catch(console.log);
+        id('register').onclick = form;
+        id('apply2').onclick = application;
 
     });
-
-    function updateNav(response) {
-        if (response.validated == "true" && response.account.type == "Business") {
-            id("provider").classList.remove("hidden");
-        }
-    }
 
     function form() {
         window.location = "signup";
