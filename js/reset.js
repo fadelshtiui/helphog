571382ecf62d6d152396cc8559c370e106d64fa6
Fadel Shtiui <fadelshtiui@gmail.com>
2021-03-27 17:41:11 -0700

    use global modal

--- js/reset.js
+++ js/reset.js
@@ -79,15 +79,31 @@
                id("confirm-password-error").innerText = "";
           }
 
-          if (response.numbererror == "true") {
-               alert("Error occured. Please retry clicking on the reset link you received.");
+          if (response.numbererror == "true" || response.numbererror == "empty") {
+               id('warning-message').innerText = "Error occured. Please retry clicking on the reset link you received."
+               let warningIcon = document.createElement('i')
+               warningIcon.classList.add('fas', 'fa-exclamation-circle', 'warning')
+               id('first').innerHTML = ''
+               id('first').appendChild(warningIcon)
+               document.querySelector('.modal-wrapper').classList.remove('hidden')
+
           } else {
                id("confirm-number-error").innerText = "";
           }
 
           if (response.emailerror == "" && response.passworderror == "" && response.confirmerror == "" && response.numbererror == "") {
-               alert("Your password has been successfully changed. Click okay to sign in now.");
-               window.location = "signin";
+               id('warning-message').innerText = "Error occured. Please retry clicking on the reset link you received."
+               let warningIcon = document.createElement('i')
+               warningIcon.classList.add('fas', 'fa-check-circle', 'success')
+               id('first').innerHTML = ''
+               id('first').appendChild(warningIcon)
+               document.querySelector('.modal-wrapper').classList.remove('hidden')
+
+               document.querySelector('.modal button').innerText = "OK, Login"
+               document.querySelector('.modal button').onclick = function () {
+                    window.location = "signin";
+               }
+
           }
 
      }
