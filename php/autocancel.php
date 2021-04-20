e133fc21761770a0a5e4507557e4c97d70644332
Fadel Shtiui <fadelshtiui@gmail.com>
2021-04-20 00:03:32 -0700

    fix time bug where timezone offset would be added more than once

--- php/autocancel.php
+++ php/autocancel.php
@@ -65,8 +65,8 @@ foreach ($result as $row) {
             $payment_info = payment($order_number);
 
             $stripe->paymentIntents->cancel(
-              trim($payment_info->intent),
-              []
+                trim($payment_info->intent),
+                []
             );
 
             if ($client_email != "") {
@@ -75,7 +75,7 @@ foreach ($result as $row) {
                 $timezone = "";
                 $stmnt = $db->prepare("SELECT work_phone, firstname, timezone FROM {$DB_PREFIX}login WHERE email = ?;");
                 $stmnt->execute(array($client_email));
-                foreach($stmnt->fetchAll() as $row) {
+                foreach ($stmnt->fetchAll() as $row) {
                     $primary_work_phone = $row['work_phone'];
                     $name = ' ' . $row['firstname'];
                     $timezone = $row['timezone'];
@@ -83,27 +83,25 @@ foreach ($result as $row) {
 
                 $utc = new DateTime(date('Y-m-d H:i:s', strtotime($schedule)), new DateTimeZone('UTC'));
                 $utc->setTimezone(new DateTimeZone($timezone));
-                $schedule = $utc->format('F j, Y, g:i a');
 
-                if($provider_never_started){
-                    send_email($client_email, "no-reply@helphog.com", "Task Cancelled", partner_never_started($service, $order_number, $schedule, $name));
-                }else{
-                    send_email($client_email, "no-reply@helphog.com", "Task Cancelled", noPartnersFound($service, $order_number, $schedule, $name));
+                if ($provider_never_started) {
+                    send_email($client_email, "no-reply@helphog.com", "Task Cancelled", partner_never_started($service, $order_number, $utc->format('F j, Y, g:i a'), $name));
+                } else {
+                    send_email($client_email, "no-reply@helphog.com", "Task Cancelled", noPartnersFound($service, $order_number, $utc->format('F j, Y, g:i a'), $name));
                 }
 
-                sendTextProvider($service, $order_number, $primary_work_phone, $schedule, $provider_never_started);
-
+                sendTextProvider($service, $order_number, $primary_work_phone, $utc->format('F j, Y, g:i a'), $provider_never_started);
             }
 
 
-            foreach(explode(',', $secondary_providers) as $email){
+            foreach (explode(',', $secondary_providers) as $email) {
                 if ($email != "") {
                     $work_phone = "";
                     $name2 = "";
                     $timezone = "";
                     $stmnt = $db->prepare("SELECT work_phone, firstname, timezone FROM {$DB_PREFIX}login WHERE email = ?;");
                     $stmnt->execute(array($email));
-                    foreach($stmnt->fetchAll() as $row) {
+                    foreach ($stmnt->fetchAll() as $row) {
                         $work_phone = $row['work_phone'];
                         $name2 = ' ' . $row['firstname'];
                         $timezone = $row['timezone'];
@@ -111,54 +109,49 @@ foreach ($result as $row) {
 
                     $utc = new DateTime(date('Y-m-d H:i:s', strtotime($schedule)), new DateTimeZone('UTC'));
                     $utc->setTimezone(new DateTimeZone($timezone));
-                    $schedule = $utc->format('F j, Y, g:i a');
 
-                    sendTextProvider($service, $order_number, $work_phone, $schedule, $provider_never_started);
+                    sendTextProvider($service, $order_number, $work_phone, $utc->format('F j, Y, g:i a'), $provider_never_started);
 
-                    if($provider_never_started){
-                        send_email($email, "no-reply@helphog.com", "Task Cancelled", partner_never_started($service, $order_number, $schedule, $name2));
-                    }else{
-                        send_email($email, "no-reply@helphog.com", "Task Cancelled", noPartnersFound($service, $order_number, $schedule, $name2));
+                    if ($provider_never_started) {
+                        send_email($email, "no-reply@helphog.com", "Task Cancelled", partner_never_started($service, $order_number, $utc->format('F j, Y, g:i a'), $name2));
+                    } else {
+                        send_email($email, "no-reply@helphog.com", "Task Cancelled", noPartnersFound($service, $order_number, $utc->format('F j, Y, g:i a'), $name2));
                     }
-
                 }
             }
 
             $name3 = "";
             $stmnt = $db->prepare("SELECT firstname, timezone FROM {$DB_PREFIX}login WHERE email = ?;");
             $stmnt->execute(array($customer_email));
-            foreach($stmnt->fetchAll() as $row) {
+            foreach ($stmnt->fetchAll() as $row) {
                 $name3 = ' ' . $row['firstname'];
             }
 
             $utc = new DateTime(date('Y-m-d H:i:s', strtotime($schedule)), new DateTimeZone('UTC'));
             $utc->setTimezone(new DateTimeZone($customer_timezone));
-            $schedule = $utc->format('F j, Y, g:i a');
 
             $sql = "UPDATE {$DB_PREFIX}orders SET status = ? WHERE order_number = ?;";
             $stmt = $db->prepare($sql);
             $params = array("ac", $order_number);
             $stmt->execute($params);
 
-            sendTextCustomer($service, $order_number, $customer_phone, $schedule, $provider_never_started);
+            sendTextCustomer($service, $order_number, $customer_phone, $utc->format('F j, Y, g:i a'), $provider_never_started);
 
-            if($provider_never_started){
-                send_email($customer_email, "no-reply@helphog.com", "Task Cancelled", provider_never_started($service, $order_number, $schedule, $name3));
-            }else{
-                send_email($customer_email, "no-reply@helphog.com", "Task Cancelled", noProviderFound($service, $order_number, $schedule, $name3));
+            if ($provider_never_started) {
+                send_email($customer_email, "no-reply@helphog.com", "Task Cancelled", provider_never_started($service, $order_number, $utc->format('F j, Y, g:i a'), $name3));
+            } else {
+                send_email($customer_email, "no-reply@helphog.com", "Task Cancelled", noProviderFound($service, $order_number, $utc->format('F j, Y, g:i a'), $name3));
             }
-
         }
-
     }
-
 }
 
-function sendTextCustomer($service, $order, $phonenumber, $schedule, $provider_never_started){
+function sendTextCustomer($service, $order, $phonenumber, $schedule, $provider_never_started)
+{
     $message = '';
-    if ($provider_never_started){
+    if ($provider_never_started) {
         $message = ' was canceled because the provider has not started working on your order. The refund will appear in your bank statement within 5-10 business days.';
-    }else{
+    } else {
         $message = ' was canceled because the service provider was not located in time. We apologize for the inconvenience.';
     }
 
@@ -166,14 +159,14 @@ function sendTextCustomer($service, $order, $phonenumber, $schedule, $provider_n
     $token = '18a458337ffdfd10617571e495314311';
     $client = new Client($sid, $token);
     $client->messages->create('+1' . $phonenumber, array('from' => '+12532593451', 'body' => 'Your order for ' . $service . ' (' . $order . ') on ' . $schedule . $message));
-
 }
 
-function sendTextProvider($service, $order, $phonenumber, $schedule, $provider_never_started){
+function sendTextProvider($service, $order, $phonenumber, $schedule, $provider_never_started)
+{
     $message = '';
-    if ($provider_never_started){
+    if ($provider_never_started) {
         $message = ' was canceled because the primary provider has not started working on the order.';
-    }else{
+    } else {
         $message = ' was canceled because one or more of the secondary providers were not located for this task';
     }
 
@@ -181,5 +174,4 @@ function sendTextProvider($service, $order, $phonenumber, $schedule, $provider_n
     $token = '18a458337ffdfd10617571e495314311';
     $client = new Client($sid, $token);
     $client->messages->create('+1' . $phonenumber, array('from' => '+12532593451', 'body' => 'Your task for ' . $service . ' (' . $order . ') on ' . $schedule . $message));
-
 }
