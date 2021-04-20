718d8af1e47668e1ce22483799dedbb8de142828
Fadel Shtiui <fadelshtiui@gmail.com>
2021-04-20 01:02:14 -0700

    adjust logic; don't mark completed until 24 hours, stop orders earlier, etc.

--- php/autocomplete.php
+++ php/autocomplete.php
@@ -31,40 +31,54 @@ foreach ($result as $row) {
 
     $utc = new DateTime(date('Y-m-d H:i:s', strtotime($schedule)), new DateTimeZone('UTC'));
     $utc->setTimezone(new DateTimeZone($timezone));
-    $schedule = $utc->format('F j, Y, g:i a');
 
     $local_start = new DateTime(date('Y-m-d H:i:s', strtotime($start)), new DateTimeZone('UTC'));
 
-    $message = $service . ' (' . $order . ') on ' . $schedule . ' has been marked completed by our system. If the support provided by the provider was inadequate you can dispute the transaction by texting back DISPUTE to this number.';
+    $message = $service . ' (' . $order . ') on ' . $utc->format('F j, Y, g:i a') . ' has been marked completed by our system. If the support provided by the provider was inadequate you can dispute the transaction by texting back DISPUTE to this number.';
 
-
-    if ($wage == "per"){
-        if ($currently_paused == 'y' && minutes_since($row['schedule']) > 1435){
+    if ($wage == "per") {
+        // condition:   order has been started and it's been more than the ordered time
+        // action:      pause order if necessary, and then stop the order
+        // reasoning:   worked time doesn't matter since it's a flat rate service
+        if ($status == 'st' && minutes_since($schedule) > ($duration * 60)) { 
+            if ($currently_paused == 'y') {
+                resume_order($order);
+            }
+            start_stop_order($order);
+        }
+        // condition:   order has been stopped and 24 hours have passed since scheduled order time
+        // action:      mark order completed
+        // reasoning:   this gives provider time to upload receipts
+        if ($status == 'en' && minutes_since($schedule) > 1440) { 
+            mark_completed($order, $message);
+        }
+    } else{
+        
+        // condition:   order was left on pause and 24 hours have passed since scheduled order time
+        // action:      resume the order, stop it, and mark it completed
+        // reasoning:   can't check worked time because that doesn't get updated until you resume
+        if ($currently_paused == 'y' && minutes_since($schedule) > 1440) { 
             resume_order($order);
             start_stop_order($order);
             mark_completed($order, $message);
-        }else if ($status == 'en' && minutes_since($row['schedule']) > 1435) {
-            mark_completed($order, $message);
-        }else if ($status == 'st' && minutes_since($row['schedule']) > 1435) {
-            start_stop_order($order);
-            mark_completed($order, $message);
+        } else {
+            $worked_time = payment($order)->worked_time;
+            // condition:   order was started and provider has worked (not including paused time) more than ordered duration
+            // action:      resume the order if necessary, and then stop it
+            // reasoning:   provider won't get paid for more work than this anyways
+            if ($status == 'st' && $worked_time > ($duration * 60)) {
+                if ($currently_paused == 'y') {
+                    resume_order($order);
+                }
+                start_stop_order($order);
+            }
+            // condition:   order is stopped and 24 hours have passed since scheduled order time
+            // action:      mark order completed
+            // reasoning:   this gives provider time to upload receipts
+            if ($status == 'en' && minutes_since($schedule) > 1440) {
+                mark_completed($order, $message);
+            }
         }
-
-    }else{
-        $now = new DateTime(gmdate('Y-m-d H:i:s'));
-        $diff = strtotime($now->format('Y-m-d H:i:s')) - strtotime($local_start->format('Y-m-d H:i:s'));
-        $workedTime = payment($order)->worked_time;
-        if ($currently_paused == 'y' && minutes_since($schedule) >= 1435){
-            resume_order($order);
-            start_stop_order($order);
-            mark_completed($order, $message);
-        }else if ($status == 'en' && ($workedTime/3600 >= $duration || minutes_since($schedule) >= 1435)){
-            mark_completed($order, $message);
-        }else if ($status == 'st'  && ($workedTime/3600 >= $duration || minutes_since($schedule) >= 1435)){
-            start_stop_order($order);
-            mark_completed($order, $message);
-        }
+        
     }
 }
-
-?>
