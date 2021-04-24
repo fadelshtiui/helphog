aeb01968db3aaf9c73c8d4e022df3e7c6a4af13f
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2021-04-24 16:04:59 -0700

    taxjar api key

--- php/payment.php
+++ php/payment.php
@@ -53,7 +53,7 @@ function calculateTax($price, $taxCode, $order_info): array
         return array(0, 0);
     }
 
-    $client = TaxJar\Client::withApiKey('69bf1c893fbd334f69cbeab198128f8f');
+    $client = TaxJar\Client::withApiKey('343de556f72124f9c744cafe18ab2bfc');
     $order_taxes = $client->taxForOrder([
         'to_zip' => $order_info->zip,
         'to_state' => $order_info->state,
