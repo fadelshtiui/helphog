a0cbeccb5aece637b2bac6436e62f1a101bf6247
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2021-12-21 23:31:31 -0800

    ios notification

--- php/pause.php
+++ php/pause.php
@@ -1,14 +1,31 @@
 <?php
 include 'common.php';
+include 'constants.php';
 if (isset($_POST["ordernumber"]) && isset($_POST["session"])) {
+
+
+
     $order = trim($_POST["ordernumber"]);
     $session = trim($_POST["session"]);
-    
+
     if (validate_provider($order, $session)) {
         pause_order($order);
+
+        $db = establish_database();
+
+
+        $service = '';
+        $customer_email = '';
+        $stmnt = $db->prepare("SELECT service, customer_email FROM {$DB_PREFIX}orders WHERE order_number = ?;");
+    	$stmnt->execute(array($order));
+    	foreach ($stmnt->fetchAll() as $row) {
+    		$service = $row['service'];
+    		$customer_email = $row['customer_email'];
+    	}
+
+        ios_customer_notification($customer_email, "Work Paused By Provider", $service . " (" . $order . ")", $order, "#1ecd97");
     }
-    
+
 
 }
 ?>
-
