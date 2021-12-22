6436ab2ce1a9901e106b09ddc96a73e4971aae43
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2021-12-21 23:31:03 -0800

    ios notification

--- php/resume.php
+++ php/resume.php
@@ -1,13 +1,27 @@
 <?php
 include 'common.php';
 if (isset($_POST["ordernumber"]) && isset($_POST["session"])) {
-    
+
     $order = trim($_POST["ordernumber"]);
     $session = trim($_POST["session"]);
-    
+
     if (validate_provider($order, $session)) {
         resume_order($order);
+
+        $db = establish_database();
+
+
+        $service = '';
+        $customer_email = '';
+        $stmnt = $db->prepare("SELECT service, customer_email FROM {$DB_PREFIX}orders WHERE order_number = ?;");
+    	$stmnt->execute(array($order));
+    	foreach ($stmnt->fetchAll() as $row) {
+    		$service = $row['service'];
+    		$customer_email = $row['customer_email'];
+    	}
+
+        ios_customer_notification($customer_email, "Work Resumed By Provider", $service . " (" . $order . ")", $order, "#1ecd97");
     }
-    
+
 }
 ?>
