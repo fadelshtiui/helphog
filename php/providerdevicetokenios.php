4e877c70ee94608dfce447d32a61bea652d502d9
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2021-12-21 18:30:58 -0800

    add provider device token

---
+++ php/providerdevicetokenios.php
@@ -0,0 +1,42 @@
+<?php
+
+include 'common.php';
+if (isset($_POST["token"]) && isset($_POST["session"])) {
+
+    $token = $_POST["token"];
+
+    $errors = new \stdClass();
+
+    $db = establish_database();
+
+    $session = trim($_POST['session']);
+
+    if (check_session($session)) {
+
+        $stmnt = $db->prepare("SELECT iostokenprovider FROM {$DB_PREFIX}login WHERE session = ?;");
+        $stmnt->execute(array($session));
+        foreach($stmnt->fetchAll() as $row) {
+            $tokens = $row['iostokens'];
+
+        }
+
+        if (strpos($tokens, $token) == false) {
+            $tokens = $tokens . "," . $token;
+            $sql2 = "UPDATE {$DB_PREFIX}login SET iostokenprovider = ? WHERE session = ?";
+            $stmt2 = $db->prepare($sql2);
+            $params = array($tokens, $session);
+            $stmt->execute($params);
+        }
+
+        $errors->sessionerror = "false";
+
+    } else {
+
+        $errors->sessionerror = "true";
+        header('Content-type: application/json');
+        print json_encode($errors);
+
+    }
+}
+
+?>
