f6a4f09ea31a0996f507277e6c31e6292aad9e5c
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2022-02-02 18:31:47 -0800

    Resolve error if entered address doesnt have a city, state, or zip

--- php/updateproviderinfo.php
+++ php/updateproviderinfo.php
@@ -3,7 +3,7 @@
 include 'common.php';
 
 if (isset($_POST["distance"]) && isset($_POST["city"]) && isset($_POST["state"]) && isset($_POST["zip"]) && isset($_POST["session"]) && isset($_POST["address"])) {
-    
+
     $db = establish_database();
 
     $distance = trim($_POST['distance']);
@@ -12,29 +12,29 @@ if (isset($_POST["distance"]) && isset($_POST["city"]) && isset($_POST["state"])
     $state = trim($_POST['state']);
     $city = trim($_POST['city']);
     $address = trim($_POST['address']);
-    
+
     $user = get_user_info($session);
     $email = $user['email'];
-    
+
     if ($email !== '') {
-        
+
         $zip_error = "";
-        
+
         if ($distance > 100){
             $distance = 100;
         }
 
-        if ($_POST["address"] == ""){
+        if ($_POST["address"] == "" || ($_POST["city"]) == "" || ($_POST["state"]) == "" || ($_POST["zip"]) == ""){
             echo "Full address required";
             return;
         } else if ($zip_error == "") {
             $sql = "UPDATE {$DB_PREFIX}login SET radius = ?, work_address = ?, work_city = ?, work_state = ?, work_zip = ? WHERE session = ?";
             $stmt = $db->prepare($sql);
             $params = array($distance, $address, $city, $state, $zip, $user['session']);
-            $stmt->execute($params); 
+            $stmt->execute($params);
         }
-        
+
     }
-   
+
 }
 ?>
