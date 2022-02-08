aa04265fff9b208b8cc9767301093d2392e9486e
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2022-02-07 21:17:42 -0800

    fix grant priv issue

--- php/toggleaccount.php
+++ php/toggleaccount.php
@@ -6,36 +6,42 @@ $response = new stdClass();
 $response->error = "";
 
 if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["action"])) {
-    
+
 	$email = trim($_POST["email"]);
 	$password = trim($_POST["password"]);
 	$action = trim($_POST["action"]);
-	
+
 	$account_type;
+	$banned = '';
+	$cancels = 0;
 	if ($action == "add") {
 	    $account_type = "Business";
+	    $banned = 'n';
+	    $cancels = 0;
 	} else if ($action == "remove") {
 	    $account_type = "Personal";
+	    $banned = 'y';
+	    $cancels = 3;
 	}
-	
+
 	if (hash_equals($password, 'yoburger')) {
 	// if (hash_equals($password, 'HrgYlM&gkHqu&5QdFV_lT!_WcKF|jNBRsM=xcm?4df!r*keHJF$YAhm$T#k4?lhdV_Ht&b3=ed+5v=jg3p@GLNAC4nND^rt@Ew*vPM6oDHa?GWm#E6wIVjXJlvj99?s-?WeOZuByVbP=Yw^cmTJ0Xv^-KJYLXy?q6HAWA=p=oxH4gQP@qKF1R%=EIwQ3Zt0vSMzct?oF1jn|J1njw73g78jE#TVYS813I=a7jvdLjn0+VK$WqIM5G+62%zuuPBKS')) {
 		$db = establish_database();
-		
-		$sql = "UPDATE {$DB_PREFIX}login SET type = ? WHERE email = ?";
+
+		$sql = "UPDATE {$DB_PREFIX}login SET type = ?, cancels = ?, banned = ? WHERE email = ?";
         $stmt = $db->prepare($sql);
-        $params = array($account_type, $email);
+        $params = array($account_type, $cancels, $banned, $email);
         $stmt->execute($params);
-        
+
 	} else {
-	    
+
 	    $response->error = 'access denied';
-	    
+
 	}
 } else {
-    
+
     $response->error = "missing parameters";
-    
+
 }
 
 header('Content-type: application/json');
