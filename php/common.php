7080cd1b46d702ef4b5fef89059242730d89f155
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2022-02-08 10:14:05 -0800

    provider app notifcation for new tasks

--- php/common.php
+++ php/common.php
@@ -247,6 +247,102 @@ function ios_provider_notification($email, $title, $body, $thread_id, $color){
 	    }
 	}
 }
+function send_new_task_ios($email, $title, $body, $color, $service, $service, $local_time, $duration, $commute, $location, $price, $partners, $message, $ordernumber, $secret_key){
+    include 'constants.php';
+	$db = establish_database();
+
+	$tokens = "";
+
+	$stmnt = $db->prepare("SELECT iostokenprovider FROM {$DB_PREFIX}login WHERE email = ?;");
+	$stmnt->execute(array($email));
+	foreach ($stmnt->fetchAll() as $row) {
+		$tokens = $row['iostokenprovider'];
+	}
+
+	if ($commute != ''){
+	    $commute = substr($commute,19);
+	}
+
+	if ($tokens != ''){
+	    $keyfile = 'AuthKey_MR5L97ZV2R.p8';               # <- Your AuthKey file
+        $keyid = 'MR5L97ZV2R';                            # <- Your Key ID
+        $teamid = '9R7PSB424V';                           # <- Your Team ID (see Developer Portal)
+        $bundleid = 'HelpHog-LLC.HelpHog-Provider';                # <- Your Bundle ID
+        $url = 'https://api.push.apple.com';  # <- development url, or use http://api.push.apple.com for production environment
+
+        $message = '{"aps": {
+                    "alert": {
+                      "title" : "' . $title . '",
+                      "body" : "' . $body . '"
+                    },
+                    "sound": "default",
+                    "mutable-content" : true
+                  },
+                  "color" : "' . $color . '",
+                  "type" : "new_service",
+                  "service" : "' . $service . '",
+                  "date" : "' . $local_time . '",
+                  "maxDuration" : "' . $duration . '",
+                  "pay" : "' . $price . '",
+                  "estimatedCommute" : "' . $commute . '",
+                  "location" : "' . $location . '",
+                  "message" : "' . $message . '",
+                  "secretKey" : "' . $secret_key . '",
+                  "partners" : "' . $partners . '",
+                  "order" : "' . $ordernumber . '"
+                }';
+
+
+
+        $key = openssl_pkey_get_private('file://'.$keyfile);
+
+        $header = ['alg'=>'ES256','kid'=>$keyid];
+        $claims = ['iss'=>$teamid,'iat'=>time()];
+
+        $header_encoded = base64($header);
+        $claims_encoded = base64($claims);
+
+        $signature = '';
+        openssl_sign($header_encoded . '.' . $claims_encoded, $signature, $key, 'sha256');
+        $jwt = $header_encoded . '.' . $claims_encoded . '.' . base64_encode($signature);
+
+        // only needed for PHP prior to 5.5.24
+        if (!defined('CURL_HTTP_VERSION_2_0')) {
+              define('CURL_HTTP_VERSION_2_0', 3);
+        }
+	    $seperated_tokens = explode(',', $tokens);
+	    foreach ($seperated_tokens as $token){
+
+                $http2ch = curl_init();
+                curl_setopt_array($http2ch, array(
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+                    CURLOPT_URL => "$url/3/device/$token",
+                    CURLOPT_PORT => 443,
+                    CURLOPT_HTTPHEADER => array(
+                      "apns-topic: {$bundleid}",
+                      "authorization: bearer $jwt"
+                    ),
+                    CURLOPT_POST => TRUE,
+                    CURLOPT_POSTFIELDS => $message,
+                    CURLOPT_RETURNTRANSFER => TRUE,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HEADER => 1
+                ));
+
+                 $result = curl_exec($http2ch);
+            if ($result === FALSE) {
+                throw new Exception("Curl failed: ".curl_error($http2ch));
+            }
+
+          $status = curl_getinfo($http2ch, CURLINFO_HTTP_CODE);
+        //   echo $status;
+
+
+        // header('Content-type: application/json');
+        // print json_encode($errors);
+	    }
+	}
+}
 
 function base64($data) {
     return rtrim(strtr(base64_encode(json_encode($data)), '+/', '-_'), '=');
@@ -515,7 +611,10 @@ function send_new_task_text($phonenumber, $email, $ordernumber, $price, $message
 	if ($alerts == "sms" || $alerts == "both") {
 
 		send_text($phonenumber, get_new_task_text($service, $local_time, $duration, $commute, $location, $price, $partners, $message, $email, $ordernumber, $secret_key));
+
 	}
+
+	send_new_task_ios($email, "New Service Request in your area!", $service, '#ffdd00', $service, $local_time, $duration, $commute, $location, $price, $partners, $message, $ordernumber, $secret_key);
 }
 
 function minutes_until($time)
@@ -1445,7 +1544,7 @@ function dispute_order($order_number)
 		$params = array('di', $order_disputes, $order_number);
 		$stmt->execute($params);
 
-		if ($order_disputes == 3) {
+		if ($order_disputes == 2) {
 			discord_webhook("https://discord.com/api/webhooks/939999679360401509/AC_ireOCmMno86eDRHfb8-Z-MjiN1wO5UbR6fho2heMI_28AlRs6RW1-TDY2XTohxYmk", "Mediation Required", $service, "Customer Email:", $customer_email, "Customer Phone", $customer_phone, "Order #", $order_number);
 		}
 
