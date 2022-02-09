834cceed71516e7946b980c7691a76e9dcc1b4fc
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2022-02-08 16:17:44 -0800

    provider app claimed notification

--- php/common.php
+++ php/common.php
@@ -344,6 +344,98 @@ function send_new_task_ios($email, $title, $body, $color, $service, $local_time,
 	}
 }
 
+function send_claimed_task_ios($email, $title, $body, $color, $service, $local_time, $duration, $location, $price, $message, $ordernumber, $customer_email, $customer_phone){
+    include 'constants.php';
+	$db = establish_database();
+
+	$tokens = "";
+
+	$stmnt = $db->prepare("SELECT iostokenprovider FROM {$DB_PREFIX}login WHERE email = ?;");
+	$stmnt->execute(array($email));
+	foreach ($stmnt->fetchAll() as $row) {
+		$tokens = $row['iostokenprovider'];
+	}
+
+	if ($tokens != ''){
+	    $keyfile = 'AuthKey_MR5L97ZV2R.p8';               # <- Your AuthKey file
+        $keyid = 'MR5L97ZV2R';                            # <- Your Key ID
+        $teamid = '9R7PSB424V';                           # <- Your Team ID (see Developer Portal)
+        $bundleid = 'HelpHog-LLC.HelpHog-Provider';                # <- Your Bundle ID
+        $url = 'https://api.push.apple.com';  # <- development url, or use http://api.push.apple.com for production environment
+
+        $message = '{"aps": {
+                    "alert": {
+                      "title" : "' . $title . '",
+                      "body" : "' . $body . '"
+                    },
+                    "sound": "default",
+                    "mutable-content" : true
+                  },
+                  "color" : "' . $color . '",
+                  "type" : "claimed_service",
+                  "service" : "' . $service . '",
+                  "date" : "' . $local_time . '",
+                  "maxDuration" : "' . $duration . '",
+                  "pay" : "' . $price . '",
+                  "customer_phone" : "' . $customer_phone . '",
+                  "customer_email" : "' . $customer_email . '",
+                  "location" : "' . $location . '",
+                  "message" : "' . $message . '",
+                  "order" : "' . $ordernumber . '"
+                }';
+
+
+
+        $key = openssl_pkey_get_private('file://'.$keyfile);
+
+        $header = ['alg'=>'ES256','kid'=>$keyid];
+        $claims = ['iss'=>$teamid,'iat'=>time()];
+
+        $header_encoded = base64($header);
+        $claims_encoded = base64($claims);
+
+        $signature = '';
+        openssl_sign($header_encoded . '.' . $claims_encoded, $signature, $key, 'sha256');
+        $jwt = $header_encoded . '.' . $claims_encoded . '.' . base64_encode($signature);
+
+        // only needed for PHP prior to 5.5.24
+        if (!defined('CURL_HTTP_VERSION_2_0')) {
+              define('CURL_HTTP_VERSION_2_0', 3);
+        }
+	    $seperated_tokens = explode(',', $tokens);
+	    foreach ($seperated_tokens as $token){
+
+                $http2ch = curl_init();
+                curl_setopt_array($http2ch, array(
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+                    CURLOPT_URL => "$url/3/device/$token",
+                    CURLOPT_PORT => 443,
+                    CURLOPT_HTTPHEADER => array(
+                      "apns-topic: {$bundleid}",
+                      "authorization: bearer $jwt"
+                    ),
+                    CURLOPT_POST => TRUE,
+                    CURLOPT_POSTFIELDS => $message,
+                    CURLOPT_RETURNTRANSFER => TRUE,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HEADER => 1
+                ));
+
+                 $result = curl_exec($http2ch);
+            if ($result === FALSE) {
+                throw new Exception("Curl failed: ".curl_error($http2ch));
+            }
+
+          $status = curl_getinfo($http2ch, CURLINFO_HTTP_CODE);
+        //   echo $status;
+
+
+        // header('Content-type: application/json');
+        // print json_encode($errors);
+	    }
+	}
+}
+
 function base64($data) {
     return rtrim(strtr(base64_encode(json_encode($data)), '+/', '-_'), '=');
 }
@@ -1744,42 +1836,44 @@ function send_claimed_notification($order_number, $email, $type, $db, $duration)
 	if ($alerts == "sms" || $alerts == "both") {
 		send_text($client_phone, get_claimed_text($customer_email, $customer_phone, $order_number, $service, $local_date, $duration, $address, $price));
     }
+    send_claimed_task_ios($email, "Task Claimed!", 'Please contact the customer immediately to follow up on their order.', "#1ecd97", $service, $local_time, $duration, $address, $price, $message, $ordernumber,$customer_email, $customer_phone);
+
 }
 
 function get_claimed_text($customer_email, $customer_phone, $order_number, $service, $local_date, $duration, $address, $price) {
 	return 'Please contact the customer immediately to follow up on their order. Here are the order details:
 
-	Customer Contact:
-	Email: ' . $customer_email . '
-	Phone: ' . $customer_phone . '
+Customer Contact:
+Email: ' . $customer_email . '
+Phone: ' . $customer_phone . '
 
-	Order: ' . $order_number . '
-	Service: ' . $service . '
-	Date: ' . $local_date->format("F j, Y, g:i a") . '
-	Max duration: ' . $duration . '
-	Location: ' . $address . '
-	Pay: ' . $price . '
+Order: ' . $order_number . '
+Service: ' . $service . '
+Date: ' . $local_date->format("F j, Y, g:i a") . '
+Max duration: ' . $duration . '
+Location: ' . $address . '
+Pay: ' . $price . '
 
-	Message from Customer: ' . $customer_message;
+Message from Customer: ' . $customer_message;
 }
 
 function get_new_task_text($service, $local_time, $duration, $commute, $location, $price, $partners, $message, $email, $ordernumber, $secret_key) {
 	include 'constants.php';
 	return 'There\'s a new service request in your area!
 
-	Service: ' . $service . '
-	Date: ' . $local_time->format("F j, Y, g:i a") . '
-	Max duration: ' . $duration . '
-	' . $commute . '
-	Location: ' . $location . '
-	Pay: ' . $price . '
-	' . $partners . '
+Service: ' . $service . '
+Date: ' . $local_time->format("F j, Y, g:i a") . '
+Max duration: ' . $duration . '
+' . $commute . '
+Location: ' . $location . '
+Pay: ' . $price . '
+' . $partners . '
 
-	Message: ' . $message . '
+Message: ' . $message . '
 
-	Tap on the following link to obtain this job:
+Tap on the following link to obtain this job:
 
-	https://' . $SUBDOMAIN . 'helphog.com/php/accept.php?email=' . $email . '&ordernumber=' . $ordernumber . '&secret=' . $secret_key;
+https://' . $SUBDOMAIN . 'helphog.com/php/accept.php?email=' . $email . '&ordernumber=' . $ordernumber . '&secret=' . $secret_key;
 }
 
 function get_confirmation_email($order_number, $cost, $service, $name, $schedule, $customer_message, $address, $providers, $subtotal, $cancel_key, $provider)
