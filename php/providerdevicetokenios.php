4bfecc2d54b1eea3b28a2166395852adc63006fc
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2022-02-09 20:45:42 -0800

    remove sms notification if you have app notifications

--- php/providerdevicetokenios.php
+++ php/providerdevicetokenios.php
@@ -14,19 +14,37 @@ if (isset($_POST["token"]) && isset($_POST["session"]) && isset($_POST["action"]
 
     $session = trim($_POST['session']);
 
+    $alerts = "";
+	$stmnt = $db->prepare("SELECT alerts FROM {$DB_PREFIX}login WHERE ios_provider_session = ?;");
+	$stmnt->execute(array($session));
+	foreach ($stmnt->fetchAll() as $row) {
+		$alerts = $row['alerts'];
+	}
+
     if (check_session($session)) {
 
         $tokens = "";
 
         if ($action == 'in'){
+            if ($alerts == 'both'){
+	            $alerts = 'email';
+	        }
+	        //can't have alerts be none
+	        if ($alerts == 'sms'){
+	            $alerts = 'email';
+	        }
 
             $tokens = $token;
 
+        }else{
+	        if ($alerts == 'email'){
+	            $alerts = 'both';
+	        }
         }
 
-        $sql = "UPDATE {$DB_PREFIX}login SET iostokenprovider = ? WHERE ios_provider_session = ?";
+        $sql = "UPDATE {$DB_PREFIX}login SET iostokenprovider = ?, alerts = ? WHERE ios_provider_session = ?";
         $stmt = $db->prepare($sql);
-        $params = array($tokens, $session);
+        $params = array($tokens, $alerts, $session);
         $stmt->execute($params);
 
         $errors->sessionerror = "false";
