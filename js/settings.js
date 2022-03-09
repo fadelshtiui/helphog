67166045b160aba52fac2afde4831eab1014af25
vilnius1998 <43106686+vilnius1998@users.noreply.github.com>
2022-03-09 15:15:16 -0800

    pair logout button

--- js/settings.js
+++ js/settings.js
@@ -21,6 +21,8 @@
 
         id('logo').onclick = goHome;
 
+        id('logout').onclick = signOut;
+
         id('locationField').classList.add('hidden')
 
         id('edit').addEventListener('click', toggleAddressView)
@@ -252,4 +254,15 @@ function resetModal() {
     id('yes').classList.remove('hidden')
     id('no').onclick = function () { };
     id('no').innerText = ""
+}
+
+function signOut() {
+    let cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        let eqPos = cookie.indexOf("=");
+        let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+    window.location = "/";
 }
