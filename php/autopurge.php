cc1137d06d36c428ab6830b9d5cd72dcd899be83
yangste333 <yangste333@gmail.com>
2022-07-23 19:00:18 -0700

    Update autopurge.php

--- php/autopurge.php
+++ php/autopurge.php
@@ -7,6 +7,12 @@ $result = $db->query($sql);
 
 foreach ($result as $row) {
     if (minutes_since($row['timestamp']) > 1440 * 29) {
+        if ($row['banned'] == "y"){
+            $sql = "INSERT INTO {$DB_PREFIX}guestbanned VALUES (?)";
+            $stmt = $db->prepare($sql);
+            $params = array(password_hash($row["phone"], PASSWORD_DEFAULT));
+            $stmt->execute($params);
+        }
         $sql = "DELETE FROM {$DB_PREFIX}guests WHERE phone = ?";
         $stmt = $db->prepare($sql);
         $params = array($row["phone"]);
